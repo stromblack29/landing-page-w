7a75420d4a614e3fd01f7c515ab8899498283566
logic envy <nutthawut.y@synergysoftware.co.th>
2020-11-30 21:59:54 +0700

    remove static target

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'ไม้จิ้มฟัน ราคาถูก | ทำมาจากสมุนไพร',
