e2d920406087650625a931343c5f7c5ec54c1bd6
logic envy <nutthawut.y@synergysoftware.co.th>
2021-01-03 13:51:36 +0700

    update state iswhite is false

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,7 +22,7 @@ export default {
   },
   data () {
     return {
-      isWhite: true
+      isWhite: false
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ export default {
   },
   data () {
     return {
-      isWhite: true
+      isWhite: false
     }
   },
   methods: {
