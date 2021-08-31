27f21d218773e747a7181b76b816ac5774c666be
logic envy <nutthawut.y@synergysoftware.co.th>
2021-08-31 09:22:52 +0700

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'cirgameth.com-ศูนย์รวมไม้จิ้มฟัน ',
+    title: 'platongnarama.com-ศูนย์รวมไม้จิ้มฟัน ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
